67ad1d5401d41c7229fa3b2555fd7be9591afde5
Seth <s.zim517@gmail.com>
2018-04-24 10:25:21 -0400

    Modifies cart route structure

--- routes/cart.js
+++ routes/cart.js
@@ -4,7 +4,7 @@ const Product = require('../db/models/Product');
 const User = require('../db/models/User');
 const Cart = require('../db/models/Cart');
 
-router.post('/add/?product=:productId&user=:userId', (req, res, next) => {
+router.post('/add/product/:productId/user/:userId', (req, res, next) => {
     Cart.create({ userId: req.params.userId, productId: req.params.productId })
         .then(cart => res.send(cart));
 });
