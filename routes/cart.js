8117f021ed1cc356c1402b8c7874a44f9244659c
Seth <s.zim517@gmail.com>
2018-04-24 10:32:48 -0400

    Adds structure to add and retrieve cart items

--- routes/cart.js
+++ routes/cart.js
@@ -4,9 +4,24 @@ const Product = require('../db/models/Product');
 const User = require('../db/models/User');
 const Cart = require('../db/models/Cart');
 
-router.post('/add/product/:productId/user/:userId', (req, res, next) => {
+router.post('/add/user/:userId/product/:productId', (req, res, next) => {
     Cart.create({ userId: req.params.userId, productId: req.params.productId })
         .then(cart => res.send(cart));
+});
+
+router.get('/user/:userId', (req, res, next) => {
+    Cart.findAll({
+        where: {
+            userId: req.params.userId
+        }
+    })
+    .then(cart => {
+        if (cart.length > 0) {
+            res.send(cart)
+        } else {
+            res.send('No items in cart')
+        }
+    })
 });
 
 module.exports = router;
